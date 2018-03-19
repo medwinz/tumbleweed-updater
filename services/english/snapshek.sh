7c0f944805fd56d9799d99d992b93a90b88e4db2
DarthWound <darthwound@live.com>
2018-03-19 06:00:34 +0100

    Create snapshek.sh

---
+++ services/english/snapshek.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# check updates for openSUSE Tumbleweed
+#
+# @author: Alexandre Singh (https://github.com/DarthWound)
+# @license: MIT license (MIT)
+
+# AvailableTumbleweedSnapshot
+readonly ATS=$(wget -qO - "http://download.opensuse.org/tumbleweed/repo/oss/media.1/products" | grep -oP " \K\d+")
+# CurrentTumbleweedSnapshot
+readonly CTS=$(lsb_release -sr)
+# Message
+readonly txt0="Snapshot \"$ATS\" available!"
+
+while true ; do
+  if [ "$ATS" != "$CTS" ]; then
+    notify-send -i info "$txt0"
+  fi
+  sleep 45m
+done
+
+exit 0
